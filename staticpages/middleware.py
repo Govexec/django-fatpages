cbce40aa491554db816443fbc587d480ac503a75
Bryan Scott <bscott@govexec.com>
2018-03-20 14:42:15 -0400

    Remove references to NG/RF; cleanup

--- staticpages/middleware.py
+++ staticpages/middleware.py
@@ -6,28 +6,22 @@ from coffin.template import RequestContext
 
 from staticpages.views import fatpage
 from websites.models import Page
-from websites.route_fifty.views import NotFoundView as RFNotFoundView
 
 
 def page_404(request):
 
     view_vars = {
-        "page":  Page({
-        "category": None,
-        "title": '404',
+        "page": Page({
+            "category": None,
+            "title": '404',
         }, request, ad_settings=settings.DART_AD_DEFAULTS),
         'gpt': {
             'ad_unit': '/{}/{}'.format(settings.GPT_NETWORK_CODE, settings.GPT_BASE_AD_UNIT_PATH),
         }
     }
 
-
-    if settings.SITE_NAME == "Nextgov":
-        return HttpResponseNotFound(render_to_string("nextgov/content/main/404.html", view_vars, context_instance=RequestContext(request)))
-    elif settings.SITE_NAME == "Defense One":
+    if settings.SITE_NAME == "Defense One":
         return HttpResponseNotFound(render_to_string("defenseone/content/main/404.html", view_vars, context_instance=RequestContext(request)))
-    elif settings.SITE_NAME == "Route Fifty":
-        return HttpResponseNotFound(render_to_string("route_fifty/404.html", view_vars, context_instance=RequestContext(request)))
     else:
         return HttpResponseNotFound(render_to_string("content/main/404.html", view_vars, context_instance=RequestContext(request)))
 
@@ -36,12 +30,12 @@ class FatpageFallbackMiddleware(object):
     def process_response(self, request, response):
 
         if response.status_code != 404:
-            return response # No need to check for a fatpage for non-404 responses.
+            return response  # No need to check for a fatpage for non-404 responses.
         try:
             return fatpage(request, request.path_info)
         # Return the original response if any errors happened. Because this
         # is a middleware, we can't assume the errors will be caught elsewhere.
         except Http404:
             return page_404(request)
-        except:
+        except Exception:
             return page_404(request)
